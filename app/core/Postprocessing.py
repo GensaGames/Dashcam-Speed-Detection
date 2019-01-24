030864871b3d0baffbf4ec7085d61be8fd3ab590
Hennadii.Kasian <Hennadii.Kasian@globallogic.com>
2019-01-24 11:37:59 +0200

    Added smooth values in PostProcessing.

--- app/core/Postprocessing.py
+++ app/core/Postprocessing.py
@@ -5,6 +5,9 @@ import math
 import numpy as np
 from numpy import loadtxt
 
+import scipy as sp
+import scipy.ndimage
+
 from app import Settings
 
 
@@ -36,6 +39,11 @@ class Postprocessor:
             if new_val is not None:
                 x[idx] = new_val
 
+    @staticmethod
+    def smooth(x):
+        return sp.ndimage.filters.gaussian_filter1d(
+            x, 2, mode='constant')
+
 
 #####################################
 if __name__ == "__main__":
@@ -45,10 +53,10 @@ if __name__ == "__main__":
             '../../' + Settings.BUILD + '/' +
             'v1-test.txt', delimiter=" ",
             unpack=False)
-        Postprocessor.smooth_aggressive(values, 5, 2)
+        Postprocessor.smooth_aggressive(values, 5, 1)
 
         path_to = '../../' + Settings.BUILD + '/' + \
-                  'v1-test-new.txt'
+                  'v1-test-new-1.txt'
         with open(path_to, "wb") as file:
             np.savetxt(
                 file, np.round(values, 8), fmt='%.8f',
@@ -59,12 +67,12 @@ if __name__ == "__main__":
     def test_change_known_issue():
         values = loadtxt(
             '../../' + Settings.BUILD + '/' +
-            'v1-test-new.txt', delimiter=" ",
+            'v1-test-new-1.txt', delimiter=" ",
             unpack=False)
         Postprocessor.change_known_issue(values)
 
         path_to = '../../' + Settings.BUILD + '/' + \
-                  'v1-test-new.txt'
+                  'v1-test-new-2.txt'
         with open(path_to, "wb") as file:
             np.savetxt(
                 file, np.round(values, 8), fmt='%.8f',
@@ -72,4 +80,19 @@ if __name__ == "__main__":
 
     test_change_known_issue()
 
+    def test_smooth():
+        values = loadtxt(
+            '../../' + Settings.BUILD + '/' +
+            'v1-test-new-2.txt', delimiter=" ",
+            unpack=False)
+        values = Postprocessor.smooth(values)
+
+        path_to = '../../' + Settings.BUILD + '/' + \
+                  'v1-test-new-3.txt'
+        with open(path_to, "wb") as file:
+            np.savetxt(
+                file, np.round(values, 8), fmt='%.8f',
+                delimiter="\n")
+
+    test_smooth()
 
