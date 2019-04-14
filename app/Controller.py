98b4ad018cd4f1590daa1041b4643aa0590ce8f9
Hennadii.Kasian <Hennadii.Kasian@globallogic.com>
2019-04-14 21:08:14 +0300

    Updated to V250.

--- app/Controller.py
+++ app/Controller.py
@@ -297,11 +297,11 @@ if __name__ == "__main__":
     def combine_workers():
         workers = [MiniBatchWorker(
             PreprocessorParams(
-                backward=(0, 1, 2, 3), frame_y_trim=(100, -160),
+                backward=(0, 2, 4, 6), frame_y_trim=(100, -160),
                 frame_x_trim=(80, -80), frame_scale=0.55,
                 area_float=6),
             ControllerParams(
-                'OPT-V242-OPT-3D-CNN', baths=30, train_part=0.65,
+                'OPT-V250-OPT-3D-CNN', baths=30, train_part=0.65,
                 epochs=12, step_vis=80, samples=20400))]
         return workers
 
